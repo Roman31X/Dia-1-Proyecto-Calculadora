2ebf38fe83a190c810258a951c7d86bb58ba898d
Roman31X <die.haa65@gmail.com>
2024-05-29 15:35:00 -0500

    operaciones completadas

--- src/main/java/com/example/modelo/CalculoMatematico.java
+++ src/main/java/com/example/modelo/CalculoMatematico.java
@@ -1,4 +1,43 @@
 package com.example.modelo;
 
-public class CalculoMatematico {
+import java.util.List;
+
+public class CalculoMatematico implements OperacionesMatematicas{
+    protected int resultado = 0;
+    @Override
+    public int suma(List<Integer> valores) {
+        for (int valor : valores) {
+            resultado += valor;
+        }
+        return resultado;
+    }
+
+    @Override
+    public int resta(List<Integer> valores) {
+        for (int valor : valores) {
+            resultado -= valor;
+        }
+        return resultado;
+    }
+
+    @Override
+    public int multiplicacion(List<Integer> valores) {
+        for (int valor : valores) {
+            resultado *= valor;
+        }
+        return resultado;
+    }
+
+    @Override
+    public double division(List<Integer> valores) {
+        for (int valor : valores) {
+            if(valor != 0){
+                resultado /= valor;
+            }else{
+                System.out.println("No se puede dividir entre [0]");
+                break;
+            }
+        }
+        return resultado = 0;
+    }
 }
