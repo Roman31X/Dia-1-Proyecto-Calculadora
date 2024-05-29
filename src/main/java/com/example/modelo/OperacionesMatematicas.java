7dbe0305e1246302960334fd75a3e204135eea6e
Roman31X <die.haa65@gmail.com>
2024-05-29 15:30:00 -0500

    modelo aritmeticos agregados

---
+++ src/main/java/com/example/modelo/OperacionesMatematicas.java
@@ -0,0 +1,14 @@
+package com.example.modelo;
+
+import java.util.List;
+
+public interface OperacionesMatematicas {
+
+    int suma(List<Integer> valores);
+
+    int resta(List<Integer> valores);
+
+    int multiplicacion(List<Integer> valores);
+
+    double division(List<Integer> valores);
+}
