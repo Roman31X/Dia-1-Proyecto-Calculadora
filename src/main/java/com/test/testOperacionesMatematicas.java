6ffb14cac0f6dee5cd6eb9cc2d3a6d7c6d1ca6b1
Roman31X <die.haa65@gmail.com>
2024-06-03 15:05:00 -0500

    Variables decimales en test agregadas

--- src/main/java/com/test/testOperacionesMatematicas.java
+++ src/main/java/com/test/testOperacionesMatematicas.java
@@ -10,33 +10,35 @@ import static org.junit.Assert.assertEquals;
 public class testOperacionesMatematicas {
 
     public static final CalculoMatematico operacion = new CalculoMatematico();
-    public final List<Integer> valores = new ArrayList<>();
+    public final List<Integer> valorEntero = new ArrayList<>();
+    public final List<Double> valorDecimal = new ArrayList<>();
+    //Test para operaciones de números enteros
 
     @Test
     public void pruebaSumaEnteros(){
-        valores.add(5);
-        valores.add(5);
-        assertEquals(10, operacion.suma(valores));
+        valorEntero.add(5);
+        valorEntero.add(5);
+        assertEquals(10, operacion.suma(valorEntero));
     }
 
     @Test
     public void pruebaRestaEnteros(){
-        valores.add(5);
-        valores.add(4);
-        assertEquals(1, operacion.resta(valores));
+        valorEntero.add(5);
+        valorEntero.add(4);
+        assertEquals(1, operacion.resta(valorEntero));
     }
 
     @Test
     public void pruebaMultiplicacionEnteros(){
-        valores.add(5);
-        valores.add(5);
-        assertEquals(25, operacion.multiplicacion(valores));
+        valorEntero.add(5);
+        valorEntero.add(5);
+        assertEquals(25, operacion.multiplicacion(valorEntero));
     }
 
     @Test
     public void pruebaDivisionEnteros(){
-        valores.add(10);
-        valores.add(5);
-        assertEquals(2, operacion.division(valores));
+        valorEntero.add(10);
+        valorEntero.add(5);
+        assertEquals(2, operacion.division(valorEntero));
     }
 }
