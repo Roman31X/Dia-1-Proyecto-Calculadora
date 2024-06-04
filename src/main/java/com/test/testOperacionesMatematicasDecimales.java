639bbabf25607d042f669081958a66ad59649912
Roman31X <die.haa65@gmail.com>
2024-06-04 15:20:00 -0500

    Test decimal aprovado con exito

--- src/main/java/com/test/testOperacionesMatematicasDecimales.java
+++ src/main/java/com/test/testOperacionesMatematicasDecimales.java
@@ -9,34 +9,34 @@ import static org.junit.Assert.assertEquals;
 public class testOperacionesMatematicasDecimales {
 
     public static final CalculoMatematico operacion = new CalculoMatematico();
-    public final List<Double> valorDecimal = new ArrayList<>();
+    public final List<Double> valorDecimal = new ArrayList<Double>();
 
     //Test para operaciones de números decimales
     @Test
     public void pruebaSumaDecimales(){
         valorDecimal.add(5.0);
         valorDecimal.add(5.0);
-        assertEquals(10.0, operacion.sumaDecimal(valorDecimal));
+        assertEquals(10.0, operacion.sumaDecimal(valorDecimal),0.0);
     }
 
     @Test
     public void pruebaRestaDecimales(){
         valorDecimal.add(5.0);
         valorDecimal.add(4.0);
-        assertEquals(1.0, operacion.restaDecimal(valorDecimal));
+        assertEquals(1.0, operacion.restaDecimal(valorDecimal),0.0);
     }
 
     @Test
     public void pruebaMultiplicacionDecimales(){
         valorDecimal.add(5.0);
         valorDecimal.add(5.0);
-        assertEquals(25.0, operacion.multiplicacionDecimal(valorDecimal));
+        assertEquals(25.0, operacion.multiplicacionDecimal(valorDecimal),0.0);
     }
 
     @Test
     public void pruebaDivisionDecimales(){
         valorDecimal.add(10.0);
         valorDecimal.add(5.0);
-        assertEquals(2.0, operacion.divisionDecimal(valorDecimal));
+        assertEquals(2.0, operacion.divisionDecimal(valorDecimal),0.0);
     }
 }
